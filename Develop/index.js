e14fe9ccd37f06fcdd467134f945f1970a7c24aa
Tarek Behairy <tbtarek@gmail.com>
2021-01-07 01:07:15 -0500

    completed for loop to append undercontainer and added function to adjust am/pm

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,22 @@
 function getLocalStorage(key) {
-  var value = localStorage.getitem(key);
+  var value = localStorage.getItem(key);
   if (value) {
     $("#text${key}").text(value);
   }
 }
 
 // JavaScript function that wraps everything
-$(document).ready(function ()) {
+$(document).ready(function () {
+
+
+  //function to adjust time (am/pm)
+  function formatAMPM(hours) {
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    return hours + ampm;
+  }
+  formatAMPM();
 
   // using moment.js to input the date for #currentday
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
@@ -16,18 +26,25 @@ $(document).ready(function ()) {
     var row = $(`<div data-time=${i} id='${i}' class="row">`);
 
     // column that will hold the time 9-5
+    var time = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
+
+    // column 2 that will hold the text area 
+    var txtArea = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);
+
+    // column 3 that will hold the save button
+    var saveBtn = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
 
 
+    // adding the columns under each row
+    row.append(time);
+    row.append(txtArea);
+    row.append(saveBtn);
 
+    //adding rows created to container
 
+    $(".container").append(row);
 
-
-
-
-
-
-
-
+    getLocalStorage(i);
 
 
   }
@@ -35,10 +52,4 @@ $(document).ready(function ()) {
 
 
 
-
-
-
-
-
-
-}
+})
